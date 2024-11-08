9bd0f2af144fc1718c2039d71de79015be6318d9
ajay-jangid-github <ajaykumarjangid957@gmail.com>
2024-11-08 15:49:56 +0530

    dark mode off

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -1,12 +1,15 @@
 //darkmode
-const chk = document.getElementById('niwax');
+{/* 
+  const chk = document.getElementById('niwax');
 chk.addEventListener('click', () => {
   chk.checked?document.body.classList.add("active-dark"):document.body.classList.remove("active-dark");
   localStorage.setItem('darkModeStatus', chk.checked);
 });
-window.addEventListener('load', (event) => {
+window.addEventListener('load', ({event}) => {
   if(localStorage.getItem('darkModeStatus')=="true"){
     document.body.classList.add("active-dark"); 
     document.getElementById('niwax').checked = true;
   }
 });
+
+  */}
